8082f68df90f0f69a7e1f6760fa2b723a8ac47ef
Adrian Fernandez Castro <adrian.fernandez.castro@rehau.com>
2022-04-11 12:33:27 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from dotenv import load_dotenv
 from datetime import datetime
-from util import search_videos, update_status
+from util import log, search_videos, update_status
 from schedule import every, run_pending
 from time import sleep
 
 load_dotenv()
-print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Application initialized')
+log('Application initialized')
 
 def cycle():
-	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Starting search cycle')
+	log('Starting search cycle')
 	videos = search_videos()
 	if videos != None: update_status(videos)
 
-	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Cycle complete')
+	log('Cycle complete')
 
 every(15).minutes.do(cycle)
 
--- util.py
+++ util.py
@@ -18,35 +18,38 @@ def search_videos():
     	search_type='video')
 
 	if len(results.items) == 0:
-		print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Found no results')
+		log('Found no results')
 		return None
 
-	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Found {len(results.items)} result(s)')
+	log(f'Found {len(results.items)} result(s)')
 	return results.items
 
 def update_status(videos: list[SearchResult]):
 	auth = OAuth1UserHandler(getenv('consumer_key'), getenv('consumer_secret'), getenv('token'), getenv('secret'))
 	twitter = API(auth)
 
-	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Updating Twitter status')
+	log('Updating Twitter status')
 
 	for video in videos:
 		if is_false_positive(video):
-			print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Skipping false positive')
+			log('Skipping false positive')
 			continue
 
 		twitter.update_status(f'Found new reaction: "{escape_formatting(video.snippet.title)}" by {video.snippet.channelTitle}\n#PTX #Pentatonix\nhttps://youtu.be/{video.id.videoId}')
 		if len(videos) > 1: sleep(5)
 
-	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Status updated')
+	log('Status updated')
 
 def is_false_positive(video: SearchResult):
 	if any(re.findall(r'ptx|pentatonix', video.snippet.title, re.IGNORECASE)):
-		print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Verified result')
+		log('Verified result')
 		return False
 	else:
-		print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] Identified false positive')
+		log('Identified false positive')
 		return True
 
 def escape_formatting(text: str):
 	return text.replace("&quot;", "\"").replace("&amp;", "&").replace("&#39;", "'")
+
+def log(message: str):
+	print(f'[{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}] {message}')
